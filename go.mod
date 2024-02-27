9fb63d3d06cf55e76e576f4f90ea58cfa999153f
Ryan Tin <9104508+ultratin@users.noreply.github.com>
2024-02-27 15:13:24 +0800

    generics

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ultratin/adder
 
 go 1.22.0
+
+require golang.org/x/exp v0.0.0-20240222234643-814bf88cf225 // indirect
--- solution.go
+++ solution.go
@@ -1,8 +1,14 @@
 // Package ch10_solution contains the solution to the exercises in chapter 10 of Learning Go, 2nd edition.
 package adder
 
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // Add is a function that takes in two integer values and returns their sum.
 // It follows the rules specified by https://www.mathsisfun.com/numbers/addition.html
-func Add(a, b int) int {
+func Add[T Number](a, b T) T {
 	return a + b
 }
